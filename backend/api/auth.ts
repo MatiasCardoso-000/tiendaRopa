f0316c40ee1492f541c25bc36d5d280ba7c85798
MatiasCardoso-000 <matias.cardoso3001@gmail.com>
2025-05-11 16:47:21 -0300

    feat: Implement authentication requests for user registration and login

--- backend/api/auth.ts
+++ backend/api/auth.ts
@@ -1,10 +1,11 @@
 import axios from "./axios";
+import {User} from '../../client/src/types/user.interface'
 
-export const registerRequest = async (user) => {
+export const registerRequest = async (user: User) => {
   return await axios.post("/registro", user);
 };
 
-export const loginRequest = async (user) => {
+export const loginRequest = async (user: User) => {
   return await axios.post("/login", user);
 };
 
